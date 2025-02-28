82fb276c7ede7084908e7f458498f53b7f673190
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-02-28 22:16:24 +0530

    Updating Frontend

--- StoringNameMongoDB/frontend/src/App.jsx
+++ StoringNameMongoDB/frontend/src/App.jsx
@@ -1,18 +1,38 @@
-import { useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import axios from "axios"
 function App() {
-  async function GetRequest() {
-    const response = await axios.get("http://localhost:5000")
-    alert(response.data.msg + " -> Status " + response.status )
-    
+  const [ username , setUsername ] = useState("")
+  const [ id , setID ] = useState("")
+  async function CreateUser(e) {
+    e.preventDefault();
+    const response = await axios.post("http://localhost:5000/user" , {username})
+    alert(response.data.msg)
   }
-  useEffect( () => {
-    GetRequest()
-  } )
+  async function GetUser(){
+    const response = await axios.get(`http://localhost:5000/user/?id=${id}`)
+    alert(response.data.msg)
+  }
   return (
     <>
-      
+      <input  
+        type='text' 
+        name='id' 
+        placeholder='Enter ID'
+        value={id}
+        onChange={(e) => setID(e.target.value)}  
+      />
+      <button onClick={GetUser}>^</button>
+      <form onSubmit={CreateUser}>
+        <input 
+          type='text' 
+          name='username' 
+          placeholder='Enter your username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}  
+        />
+        <button type='submit'>Submit</button>
+      </form>
     </>
   )
 }
