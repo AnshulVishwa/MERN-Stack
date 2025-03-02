290d8a1201a00e9c2c0591a875f204c53c013298
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-02 06:54:05 +0530

    Styling Form Page

--- LoginSignup/frontend/src/App.jsx
+++ LoginSignup/frontend/src/App.jsx
@@ -1,31 +1,58 @@
 import { useState } from 'react'
+import background from "./images/background.jpg"
 import './App.css'
 
 function App() {
   const [ username , setUsername ] = useState("")
   const [ password , setPassword ] = useState("")
-  const [ type , setType ] = useState("text")
-  const [ eyes , setEyes ] = useState("")
+  const [ eyes , setEyes ] = useState(false)
   return (
     <>
+      <img className='background' src={background} alt="" />
       <form>
-        <div className="username">
-          <label>Username</label>
-          <input 
-            type="text" 
-            name='username'
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}  
-          />
-        </div>
-        <div className="password">
-          <label>Password</label>
-          <input 
-            type={type} 
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}  
-          />
+        <img src={background} className="image" />
+        <div className="formContent">
+
+          <h3>Signup</h3>
+          <div className="username">
+            <span>
+              <i className="fa-solid fa-user"/>
+            </span>
+            <input 
+              type={"text"} 
+              name='username'
+              value={username}
+              placeholder='Username'
+              onChange={(e) => setUsername(e.target.value)}  
+            />
+          </div>
+          <div className="password">
+            <span>
+              <i
+                className={ (eyes) ? "fa-solid fa-lock-open" : "fa-solid fa-lock" } 
+                onClick={() => setEyes(!eyes)} 
+              />
+            </span>
+            <input 
+              type={(eyes) ? "text" : "password"} 
+              name='password'
+              value={password}
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}  
+            />
+          </div>
+          <div className="rememberMe">
+            <div className="input">
+              <input type="checkbox" />
+              <span>Remember me</span>
+            </div>
+            <div className="login">
+              <span>Login</span>
+            </div>
+          </div>
+          <div className="btn">
+            <button>Signup</button>
+          </div>
         </div>
       </form>
     </>
