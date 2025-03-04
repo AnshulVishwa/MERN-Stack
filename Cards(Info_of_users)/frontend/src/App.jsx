cf5239fddf5e6864a34988521c7475d16f94e6d4
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-04 17:43:22 +0530

    Setting up route

--- Cards(Info_of_users)/frontend/src/App.jsx
+++ Cards(Info_of_users)/frontend/src/App.jsx
@@ -3,14 +3,32 @@ import './App.css'
 import logo from "./assets/logo.png"
 import FormSubmition from './Service/authentication'
 import axios from "axios"
+import Loader from './Components/Loader'
 
 function App() {
+  const [ res , setRes ] = useState()
+  const [ loader , setLoader ] = useState(false)
+  async function onPageLoadreq() {
+    setLoader(true)
+    setRes(await axios.get("http://localhost:5000/"))
+    setLoader(false)
+  }
+  useEffect( () => {
+    onPageLoadreq()
+  } , [] )
+  useEffect( () => {
+    if( loader ){
+      document.querySelectorAll("section").forEach((each) => each.style.display = "none" )
+    }
+    else{
+      document.querySelectorAll("section").forEach((each) => each.style.display = "flex" )
+    }
+  } , [loader] )
 
   function handleSubmit(e){
     e.preventDefault()
     alert(FormSubmition(info))
-    const response = axios.post( "localhost:5000/info" , { info } )
-    (response.data.status == "OK")
+    const response = axios.post( "http://localhost:5000/info" , { info } )
   }
 
   const content = [ "Name" , "Date of Birth" , "Age" , "Gender" , "Profession" , "About Yourself" ]
@@ -29,6 +47,7 @@ function App() {
   } , [info.dob] )
   return (
     <>
+      { loader && <Loader /> }
       <section className="section1 flex">
         <div className="logo-content flex">
           <h1>Welcome to</h1>
