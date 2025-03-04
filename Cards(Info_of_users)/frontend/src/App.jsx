0c9cfc319520fd374711fee25480e940b47d3b15
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-04 07:00:37 +0530

    Applying styles

--- Cards(Info_of_users)/frontend/src/App.jsx
+++ Cards(Info_of_users)/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import logo from "./assets/logo.png"
 
 function App() {
+  const content = [ "Name" , "Date of Birth" , "Age" , "Gender" , "Profession" , "About Yourself" ]
   return (
     <>
       <section className="section1 flex">
@@ -12,6 +13,25 @@ function App() {
             <span className='textLogo'>Cardox</span>
           </div>
           <span>Where we store information of our users, we definitely keep a check on our user's privacy and keep their trust.</span>
+        </div>
+      </section>
+
+      <section className="section2 flex">
+        <div className="innerDiv flex">
+          <h1>Your Information</h1>
+          <main>
+            {
+              content.map( ( v , i ) => (
+                <div key={i} className={`${v.replaceAll(" " , "")} content`}>
+                  <label>{v} :</label>
+                  <input className='textBox'></input>
+                </div>
+              ) )
+            }
+          </main>
+          <div className="sumbitBtn">
+            <button>Submit</button>
+          </div>
         </div>
       </section>
     </>
