53c6185fe83e108ec2b13fa80a4339e9eb8776d6
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-07 21:23:50 +0530

    Improving Frontend

--- Cards(Info_of_users)/frontend/src/Components/Home.jsx
+++ Cards(Info_of_users)/frontend/src/Components/Home.jsx
@@ -2,26 +2,45 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Navbar from './Navbar'
 import "./Styles/home.css"
+import back_image from "../assets/home.jpg"
 
 function Home() {
   const [allUsers , setAllUsers] = useState()
+  const [users , setUsers] = useState()
+
   async function GetReq() {
     const response = await axios.get( "http://localhost:5000/info" )
     if( response ){
+      console.log(response.data.users)
       setAllUsers(response.data.users)
+      setUsers(() => response.data.users.filter( (v , i) => ( i < 6 ) ))
+        console.log(users)
     }
   }
+
   useEffect( () => {
     GetReq()
-    console.log(allUsers)
   } , [] )
   return (
     <>
     <div className="mainDiv flex">
+        <img src={back_image} />
         <header className='flex'>
           <Navbar />
         </header>
-        <div className="middleDiv"></div>
+        <div className="middleDiv flex">
+          {
+            users?.map( (v , i) => (
+              <div className="card flex">
+                <div className="cardContent flex">
+                  <span className="heading cardInfo">{v.username}</span>
+                  <span className="age cardInfo">{v.age}</span>
+                  <span className="profession cardInfo">{v.profession}</span>
+                </div>
+              </div>
+             ) )
+          }
+        </div>
     </div>
     </>
   )
