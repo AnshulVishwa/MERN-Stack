96c7dfa65d44f6dfcd27a6c26f05c03f6dfa3bca
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-09 14:02:12 +0530

    Setting 404Error Page

---
+++ Cards(Info_of_users)/frontend/src/Components/Error404.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Styles/error.css"
+
+function Error404() {
+  return (
+    <>
+        <div className="msg">
+            <h1>404 Error</h1>
+            <h2>Page not Found</h2>
+        </div>
+    </>
+  )
+}
+
+export default Error404
--- Cards(Info_of_users)/frontend/src/main.jsx
+++ Cards(Info_of_users)/frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import Home from './Components/Home.jsx';
 import Card from './Components/Card.jsx';
+import Error404 from './Components/Error404.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Route index element={ <App/> } />
       <Route path='home' element={ <Home /> }></Route>
       <Route path='card' element={ <Card /> }></Route>
+      <Route path='/:404' element={ <Error404 /> }></Route>
     </Routes>
   </BrowserRouter>
 );
