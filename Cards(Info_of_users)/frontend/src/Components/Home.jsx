7b982b9780ebfd6f106984d15b2f0833063e2ffb
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-13 13:12:01 +0530

    token

--- Cards(Info_of_users)/frontend/src/Components/Home.jsx
+++ Cards(Info_of_users)/frontend/src/Components/Home.jsx
@@ -9,6 +9,7 @@ import { useNavigate } from "react-router";
 function Home() {
   const navigate = useNavigate()
   const [users , setUsers] = useState()
+  const [ myCards , setMyCards ] = useState([""])
   const [ mainCard , setMainCard ] = useState()
 
   async function GetReq() {
@@ -19,17 +20,30 @@ function Home() {
           alert(res.data.msg)
           navigate("/")
         }
-        setUsers(res.data.users);
+        else{
+          setUsers(res.data.users);
+          setMyCards(res.data.myCards)
+        }
     })
     .catch((err) => {
         console.error("Error fetching users:", err);
     });
 }
 
-
   useEffect( () => {
     GetReq()
   } , [] )
+
+  useEffect(() => {
+    try {
+      const ids = myCards?.ArrayOfObj?.map((v) => v.id) ?? []; // Ensure it's always an array
+      const filter =  users?.filter( (v) => ids.includes(v._id.toString()) )
+      filter = filter.map( ( v ) => v.bool = true )
+      setUsers( (prev) => ( [ ...prev , ...filter ] ) )
+    } catch (err) { }
+  }, [myCards, setUsers]);
+  
+
   return (
     <>
     <div className="mainDiv flex">
@@ -43,6 +57,9 @@ function Home() {
               users?.map( (v , i) => (
                 <div key={i} onClick={() => setMainCard(v)} className="card flex">
                   <div className="cardContent flex">
+                    {
+                       v.bool && <div>Mine</div>
+                    }
                     <span className="heading cardInfo">{v.username}</span>
                     <span className="age cardInfo">{v.age}</span>
                     <span className="profession cardInfo">{v.profession}</span>
