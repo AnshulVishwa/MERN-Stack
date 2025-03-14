aafca03b9d51f16fdba2bf8b7dd5975b987695e6
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-14 05:46:53 +0530

    Initializing new Project

--- Archive_Excel_Data/frontend/src/App.jsx
+++ Archive_Excel_Data/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import background from "./assets/background.jpg"
 import folder from "./assets/folder.png"
+import axios from "axios"
 
 function App() {
   const [ filename , setFilename ] = useState()
@@ -18,11 +19,18 @@ function App() {
       const reader = new FileReader
       reader.readAsArrayBuffer(file)
 
-      reader.onload = function(e) {
+      reader.onload = async function (e) {
         const workbook = XLSX.read( e.target.result , { type : "array" } )
         const sheet = workbook.Sheets[workbook.SheetNames[0]]
         const jsonData = XLSX.utils.sheet_to_json(sheet)
-        console.log(jsonData)
+        const response = await axios.post( "http://localhost:5000/" , {jsonData} )
+        console.log("Response send")
+        if( !response ){
+          alert("Error")
+        }
+        else{
+          alert(response.data.msg)
+        }
       }
     }
     catch(err){ console.log(err) }
@@ -34,7 +42,7 @@ function App() {
       <main className="mainContent">
         <input onChange={handleSubmitFile} type="file" />
         <img src={folder} className='img' alt="" />
-        { filename }
+        { <div className='filename'>filename</div> }
         <h1>Drag or Select files </h1>
       </main>
     </>
