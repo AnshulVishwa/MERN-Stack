08139ce14f72d687c3627c43f94d841afc922854
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-04-01 06:29:25 +0530

    Initilaing Socket.io

---
+++ Initializing_Socket.io/backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(
+    cors({
+      origin: "http://localhost:5173", // Make sure no trailing slash!
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+  
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", // Your frontend URL
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data); // Broadcast message to all clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected:", socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
