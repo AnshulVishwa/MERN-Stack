5853f3066f0419b39a84ad55957c7b5f095a8285
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-04-11 09:50:28 +0530

    Updating Liv Chat Room

--- Live Chat Room/frontend/src/App.jsx
+++ Live Chat Room/frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import io from 'socket.io-client';
+import "./App.css"
 
 // Connect to backend
 const socket = io("http://localhost:5000");
@@ -9,18 +10,35 @@ function App() {
   const username = useLocation().pathname.split("/")[1];
   const [chat, setChat] = useState([]);
   const [message, setMessage] = useState("");
-
+  
   // Receive message from backend
   useEffect(() => {
     socket.on("receive_message", (data) => {
       setChat((prev) => [...prev, data]);
     });
-
-    return () => socket.off("receive_message");
+  
+    socket.on("chat_history", (data) => {    // ✅ change from "loadMessages"
+      console.log("Messages received:", data);
+      setChat(data);
+    });
+  
+    return () => {
+      socket.off("receive_message");
+      socket.off("chat_history"); // cleanup
+    };
   }, []);
 
+  useEffect( () => {
+    socket.on( "chat_history" , (data) => {
+      setChat(data);
+    } )
+    return () => socket.off("chat_history");
+  } , [username] )
+  
+
   // Send message to backend
-  const sendMessage = () => {
+  const sendMessage = (e) => {
+    e.preventDefault()
     if (message.trim() === "") return;
 
     const msgData = {
@@ -38,23 +56,25 @@ function App() {
       <h1 className="username">👤 {username}</h1>
 
       <div className="chatBox flex">
-        {chat.map((v, i) => (
-          <div key={i} className="box">
-            <div className="sender">{v.sender}</div>
-            <div className="text">{v.text}</div>
-            <div className="time">{v.time}</div>
-          </div>
-        ))}
-      </div>
+          {chat.map((v, i) => (
+            <div className={v.sender == username ? "box-receiver" : "box-sender"}>
+              <div key={i} className="box">
+                <div className="sender">{v.sender}</div>
+                <div className="text">{v.text}</div>
+                <div className="time">{v.time}</div>
+              </div>
+            </div>
+          ))}
+        </div>
 
-      <div className="sendChatDiv">
+      <form onSubmit={sendMessage} className="sendChatDiv">
         <input
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           placeholder="Type a message..."
         />
-        <button onClick={sendMessage}>Send</button>
-      </div>
+        <button type='submit'>Send</button>
+      </form>
     </>
   );
 }
