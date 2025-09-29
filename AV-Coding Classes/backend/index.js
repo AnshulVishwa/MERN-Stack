603c231ff7d338b4aa12dcc16f98023a19b8d707
anjaliraghuwanshi <raghuwanshianjali000@gmail.com>
2025-09-29 09:19:09 +0530

    Updaing URL and Logo

--- AV-Coding Classes/backend/index.js
+++ AV-Coding Classes/backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" },
+  cors: { origin: "https://classes-kappa.vercel.app/" },
 });
 
 io.on("connection", (socket) => {
--- AV-Coding Classes/frontend/src/App.jsx
+++ AV-Coding Classes/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://av-coding-classes.onrender.com");
 
 function App() {
   const [mic, setMic] = useState(true);
